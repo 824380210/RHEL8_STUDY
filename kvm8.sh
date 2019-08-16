7e231c7b05f7b7d356baec1f7de0aacb22c00f49
824380210 <824380210@qq.com>
2019-08-17 06:22:01 +0800

    run kvm8.sh to build the rhel8 virtual machine

---
+++ kvm8.sh
@@ -0,0 +1,13 @@
+virt-install --name rhel8-kvm --ram=16384 --vcpus=8  --disk path=/var/lib/libvirt/images/rhels8-kvm8.img,size=50,format=qcow2  \
+--location="http://192.168.122.1/RHEL8/" \
+--extra-args="ks=http://192.168.122.1/kvm.cfg"   \
+--os-type=linux  \
+--network bridge=virbr0,,mac=52:54:00:00:00:03 --graphics spice &
+which virt-viewer &>/dev/null
+if [ $? -ne 0 ];then
+  yum install virt-viewer -y
+fi
+virt-viewer rhel8-kvm &
+##
+# other parameter example 
+# --os-type=linux --os-variant=rhel8 
